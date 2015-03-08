bc7ec3793ef656477b53fb06126812b12d728b7d
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-08 17:25:26 -0500

    Added Custo Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :questions
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
