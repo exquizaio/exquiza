db4fbb97af6354eb26ec58117ce8835b2b218980
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-08 21:05:52 -0500

    Added Better Test Index View
    Its based on if one is an admin or not

--- app/decorators/test_decorator.rb
+++ app/decorators/test_decorator.rb
@@ -9,4 +9,33 @@ class TestDecorator < Draper::Decorator
     h.number_to_percentage(model.percent_correct, precision: 2)
   end
 
+  def display_name
+    string = "Test ##{model.id}"
+    string.prepend("#{model.user.full_name} ") if h.current_user.admin?
+    string
+  end
+
+  def allowed_actions
+    [].tap do |a|
+      if h.current_user.admin?
+        a << table_link_to('View Config', model)
+        a << table_link_to('Edit Config', h.edit_test_path(model))
+        a << table_link_to('Destroy', model, method: :delete, data: { confirm: 'Are you sure?' })
+      end
+      a << table_link_to("Start Test", h.start_test_taking_test_path(model)) if !test.started? and model.user == h.current_user
+      a << table_link_to("Continue Test", h.start_test_taking_test_path(model)) if test.started? and !model.finished? and model.user == h.current_user
+      a << table_link_to("Report", h.test_report_path(model)) if model.finished?
+    end.join.html_safe
+  end
+
+  private
+
+  def wrapped_link_to(tag=:span, *link_args)
+    h.content_tag(tag) { h.link_to(*link_args) }
+  end
+
+  def table_link_to(*link_args)
+    wrapped_link_to(:td, *link_args)
+  end
+
 end
