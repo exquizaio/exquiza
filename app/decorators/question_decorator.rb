6e65769811243ffb7e1b5a0ffe4b5999cfb56fbb
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-08 20:07:29 -0500

    Added Tag View to Report

--- app/decorators/question_decorator.rb
+++ app/decorators/question_decorator.rb
@@ -2,6 +2,16 @@ class QuestionDecorator < Draper::Decorator
   delegate_all
   decorates_association :prompt
 
+  def tag_pills
+    h.unordered_list class: "tag-list--pill" do
+      model.tags.collect {|c|
+        tag_pill(c)
+      }.join.html_safe
+    end
+  end
 
+  def tag_pill(tag, node: :li, **options)
+    h.content_tag node, tag, class: "tag-pill", **options
+  end
 
 end
